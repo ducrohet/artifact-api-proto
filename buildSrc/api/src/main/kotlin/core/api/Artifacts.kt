dbe051cbb7fbe02215b4951b71984fa3572cbb36
Xavier Ducrohet <xav@google.com>
2019-08-09 12:33:15 -0700

    Clean up enum
    Path sensitivty and normalizer is not used anymore.

--- buildSrc/api/src/main/kotlin/core/api/Artifacts.kt
+++ buildSrc/api/src/main/kotlin/core/api/Artifacts.kt
@@ -11,28 +11,16 @@ import org.gradle.api.tasks.FileNormalizer
 import org.gradle.api.tasks.PathSensitivity
 
 /**
- * Provider of input information such as [PathSensitivity] and [FileNormalizer].
- *
- * This is attached to an artifact enum to automatically configure the inputs of tasks that
- * transform artifact
- *
- */
-interface InputInfoProvider {
-    val sensitivity: PathSensitivity?
-    val normalizer: Class<out FileNormalizer>?
-}
-
-/**
  * Base for artifacts only handling a single file or directory
  */
-interface SingleArtifactType<ValueT: FileSystemLocation, ProviderT: Provider<ValueT>> : InputInfoProvider {
+interface SingleArtifactType<ValueT: FileSystemLocation, ProviderT: Provider<ValueT>> {
     val isOutput: Boolean
 }
 
 /**
  * Base for artifacts being able to handle either a list of file or a list of directory.
  */
-interface MultiArtifactType<ValueT: FileSystemLocation, ProviderT: Provider<out Iterable<ValueT>>> : InputInfoProvider
+interface MultiArtifactType<ValueT: FileSystemLocation, ProviderT: Provider<out Iterable<ValueT>>>
 
 /**
  * Actual enum classes.
@@ -43,42 +31,29 @@ interface MultiArtifactType<ValueT: FileSystemLocation, ProviderT: Provider<out 
  */
 
 enum class SingleFileArtifactType(
-        override val sensitivity: PathSensitivity? = null,
-        override val normalizer: Class<out FileNormalizer>? = null,
         override val isOutput: Boolean = false
 ): SingleArtifactType<RegularFile, Provider<RegularFile>> {
-    MERGED_MANIFEST(sensitivity = PathSensitivity.NAME_ONLY),
-    MERGED_DEX(sensitivity = PathSensitivity.NAME_ONLY),
-    PACKAGE(sensitivity = PathSensitivity.NONE, isOutput = true)
+    MERGED_MANIFEST,
+    MERGED_DEX,
+    PACKAGE
 }
 
 enum class SingleDirectoryArtifactType(
-        override val sensitivity: PathSensitivity? = null,
-        override val normalizer: Class<out FileNormalizer>? = null,
         override val isOutput: Boolean = false
 ): SingleArtifactType<Directory, Provider<Directory>> {
-    MERGED_RESOURCES(sensitivity= PathSensitivity.NONE);
+    MERGED_RESOURCES;
 }
 
-enum class MultiFileArtifactType(
-        override val sensitivity: PathSensitivity? = null,
-        override val normalizer: Class<out FileNormalizer>? = null
-): MultiArtifactType<RegularFile, Provider<out Iterable<RegularFile>>> {
-    COMPILE_R_JAR(normalizer = ClasspathNormalizer::class.java),
-    RUNTIME_R_JAR(normalizer = CompileClasspathNormalizer::class.java),
-    DEX(sensitivity = PathSensitivity.NONE);
+enum class MultiFileArtifactType : MultiArtifactType<RegularFile, Provider<out Iterable<RegularFile>>> {
+    COMPILE_R_JAR,
+    RUNTIME_R_JAR,
+    DEX;
 }
 
-enum class MultiDirectoryArtifactType(
-        override val sensitivity: PathSensitivity? = null,
-        override val normalizer: Class<out FileNormalizer>? = null
-): MultiArtifactType<Directory, Provider<out Iterable<Directory>>> {
-    RESOURCES(sensitivity= PathSensitivity.NONE);
+enum class MultiDirectoryArtifactType : MultiArtifactType<Directory, Provider<out Iterable<Directory>>> {
+    RESOURCES;
 }
 
-enum class MultiMixedArtifactType(
-        override val sensitivity: PathSensitivity? = null,
-        override val normalizer: Class<out FileNormalizer>? = null
-): MultiArtifactType<FileSystemLocation, Provider<out Iterable<FileSystemLocation>>> {
-    BYTECODE(normalizer = CompileClasspathNormalizer::class.java);
+enum class MultiMixedArtifactType : MultiArtifactType<FileSystemLocation, Provider<out Iterable<FileSystemLocation>>> {
+    BYTECODE;
 }
